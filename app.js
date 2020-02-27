a6fb97c2e1cc5aa9cf9392f91f9027edb0496f07
username@email.com <wangqingzhou311@live.com>
2020-02-27 09:53:44 +0800

    新分支

--- app.js
+++ app.js
@@ -21,12 +21,11 @@ const usersRouter = require('./routes/users/users');
 // }
 const app = express();
 
-app.all('*', function (req, res, next) {
-    // console.log("req2,", req)
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Content-Type,Content-Length, Authorization, Accept,X-Requested-With,Cache-Control,withcredentials,ApiKey");
-    (req.method == "OPTIONS" && res.send(200)) || next() /*让options请求快速返回*/
-});
+// app.all('*', function (req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Headers", "Content-Type,Content-Length, Authorization, Accept,X-Requested-With,Cache-Control,withcredentials,ApiKey");
+//     (req.method == "OPTIONS" && res.send(200)) || next() /*让options请求快速返回*/
+// });
 
 
 
@@ -34,39 +33,27 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public/index')));
-
-app.use(function(req,res,next){
-    console.log("url,",req.url);
-    if(req.url.startsWith('/users') || req.url.startsWith('/myapp') || req.url.startsWith('/static')){
-      return next()
-    }
-    //如果访问url根路径不是user或者static就返回打包后的主页面
-    // return res.render('index', {
-    //     component: ReactDOMServer.renderToNodeStream(path.resolve(__dirname, 'public/myapp', 'index.html'))
-    // });
-    return res.sendFile(path.resolve(__dirname, 'public/index', 'index.html'))
-  })
-
-// app.get('*', function (req, res) {
-//     console.log("req1,", req)
-//     //res.sendFile(path.resolve(__dirname, 'public', 'index.html'))
-// })
+app.use(express.static(path.join(__dirname, 'public/index')));//映射public文件路径
 // app.engine('.html', ejs.__express) // 设置视图模板引擎使用，为.html
 
 // app.set('view engine', 'html'); // 设置视图引擎为html
 
-
-//映射public文件路径，项目上要使用
-// app.use('/',express.static(path.resolve('public')))
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 
-// 捕获404并转发给错误处理程序
+
 app.use(function (req, res, next) {
-    console.log("捕获404")
-    next(createError(404));
+  console.log("method,",req.method);
+  if(req.url.startsWith('/static')){
+    return next()
+  }
+  //如果访问url根路径不是user或者static就返回打包后的主页面
+  // return res.render('index', {
+  //     component: ReactDOMServer.renderToNodeStream(path.resolve(__dirname, 'public/myapp', 'index.html'))
+  // });
+  return res.sendFile(path.resolve(__dirname, 'public/index', 'index.html'))
+    // next(createError(404));// 捕获404并转发给错误处理程序
 });
 
 // 错误处理
